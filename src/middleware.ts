728b7b09f8c428236ddd3b0aef6fe5b78890aa06
Zula <alexboss12340d@gmail.com>
2026-07-22 02:58:03 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
 import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export default withAuth({
-  pages: { signIn: '/auth/login' },
-})
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next()
+  },
+  {
+    pages: { signIn: '/auth/login' },
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+)
 
 export const config = {
   matcher: ['/dashboard/:path*'],
