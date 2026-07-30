dc6e54134472a175b8fe7e379a391779b2d3db93
Zula <alexboss12340d@gmail.com>
2026-07-30 13:46:55 +0200

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -194,7 +194,7 @@ function connect() {
           op: 2,
           d: {
             token:      TOKEN,
-            intents:    33280 | 8192, // 8192 = GUILD_MESSAGE_REACTIONS
+            intents:    33280 | 8192 | 65536, // GUILD_MESSAGES + GUILD_MESSAGE_REACTIONS + MESSAGE_CONTENT
             properties: { os: 'linux', browser: 'grove-bot', device: 'grove-bot' },
           },
         }))
